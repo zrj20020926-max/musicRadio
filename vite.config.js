4c3705959ada7a3a8f3f065b9fed99a1526d9a1f
zhurongjiang <zhurongjiang@baidu.com>
2026-05-30 14:22:53 +0800

    feat: enhance effects and deploy to github pages

--- vite.config.js
+++ vite.config.js
@@ -22,5 +22,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: '/music-radio/'
+  base: process.env.VITE_BASE || '/music-radio/',
 })
